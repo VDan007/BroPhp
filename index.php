0bba9fff4cb082592bc926d5279581245f2bf1ce
DanielVarjaskéri <varjaskerid@gmail.com>
2023-05-22 00:39:43 +0200

    some basics

--- index.php
+++ index.php
@@ -1,40 +1,24 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-  
-<form action="index.php" method="post">
-
-  <label for="radius">radius:</label>
-  <input type="number" name="radius" id="radius">
-
-  <input type="submit" value="calculate">
-
-
-</form> <br>
-
-
-
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+  </head>
+  <body>
     
-
-</body>
+  </body>
 </html>
 
 <?php
-  $radius = $_POST["radius"];
-  $circumference = round(2 * pi() * $radius,2);
+ 
+    function happy_birthday($name,$age){
+      echo "happy {$age} th birthday {$name} <br>";
+    }
 
-  if($radius){
-
-    echo "Circumference is {$circumference} cm <br>";
-  }
-
+    happy_birthday("Dan",31);
+    
 ?>
 
